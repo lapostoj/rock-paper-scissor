c10ee7bb81f65ac6bd27afb109c5ef417217a35b
lapostoj <jerome.lapostolet@gmail.com>
2020-02-11 00:32:41 +0000

    Migrate gradle config to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+
+val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
+val junitPlatformVersion = "1.6.0"
+val spekVersion = "2.0.9"
+val mockkVersion = "1.9.3"
+
+group = "fr.lapostoj"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+	idea
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
+	id("org.springframework.boot") version "2.2.4.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+}
+
+idea.project {
+    vcs = "Git"
+    jdkName = "1.8"
+}
+
+tasks.compileKotlin {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
+tasks.compileTestKotlin {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
+tasks.test {
+	useJUnitPlatform {
+		includeEngines("spek2")
+	}
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.springframework.boot:spring-boot-starter-web") {
+		exclude(module = "spring-boot-starter-tomcat")
+	}
+	implementation("org.springframework.boot:spring-boot-starter-jetty")
+
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
+	testImplementation("org.spekframework.spek2:spek-dsl-jvm:${spekVersion}")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:${mockkVersion}")
+	testImplementation("org.junit.platform:junit-platform-commons:${junitPlatformVersion}")
+	testImplementation("org.junit.platform:junit-platform-engine:${junitPlatformVersion}")
+	testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:${spekVersion}")
+}

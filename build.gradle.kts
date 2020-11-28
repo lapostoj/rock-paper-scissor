1010a7fe0b13a8ab4ad35c2e57322ab8231464df
lapostoj <jerome.lapostolet@gmail.com>
2020-11-28 10:46:38 +0000

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
 val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
-val spekVersion = "2.0.12"
+val spekVersion = "2.0.14"
 val mockkVersion = "1.10.0"
-val ktlintVersion = "0.38.1"
+val ktlintVersion = "0.39.0"
 
 group = "fr.lapostoj"
 version = "0.0.1-SNAPSHOT"
 
 plugins {
     idea
-    kotlin("jvm") version "1.4.0"
-    kotlin("plugin.spring") version "1.4.0"
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("com.diffplug.spotless") version "5.1.1"
+    kotlin("jvm") version "1.4.20"
+    kotlin("plugin.spring") version "1.4.20"
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.diffplug.spotless") version "5.8.2"
 }
 
 idea.project {
